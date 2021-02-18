32180369f775180f6c6f21ab370ca67d7a074774
Arpan  Ganguli <arpanganguli@mac-mini.home>
2021-02-18 01:41:26 +0000

    initial commit

---
+++ inti_mapping.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 23:46:22 2021
+
+@author: arpanganguli
+"""
+# import packages
+
+import pandas as pd
+
+# import files
+
+url = '/Users/arpanganguli/Documents/Professional/Projects/BAMEUK/Data/data_by_ethnicity_by_utla.csv'
+df = pd.read_csv(url, thousands=',')
+print(df.head())
+
+# convert data types
+
+df[['Area','Code']] = df[['Area','Code']].astype('string')
+print(df.info())
+
+# create main data frame by grouped ethnicities
+
+df_main = df[['Area'
+             ,'Code'
+             ,'All categories: Ethnic group'
+             ,'Mixed/multiple ethnic group: Total'
+             ,'Asian/Asian British: Total'
+             ,'Black/African/Caribbean/Black British: Total'
+             ,'Other ethnic group: Total'
+                  ,'Other ethnic group: Arab'
+                  ,'Other ethnic group: Any other ethnic group']]
+print(df_main.head())
